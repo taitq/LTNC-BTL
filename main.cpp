4356a7b750d5dd7c380265fd6c29c9d3665f278b
taitq <trqt2003@gmail.com>
2022-03-08 23:17:44 +0700

    Init WindowGame

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+using namespace std;
+
+const string TITLE = "Game ban may bay";
+const int HEIGHT = 600;
+const int WIDTH = 1000;
+
+void InitWindow(SDL_Window* &window);
+int main()
+{
+    SDL_Window* window;
+    InitWindow(window);
+    SDL_Delay(2000);
+}
+void InitWindow(SDL_Window* & window)
+{
+    window = SDL_CreateWindow(TITLE.c_str(),SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, WIDTH,HEIGHT,SDL_WINDOW_SHOWN);
 }
